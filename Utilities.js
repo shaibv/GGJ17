e415c3e8db163128fcd7612eeab43a6a5c84a411
adipie <adipiek@gmail.com>
2017-01-20 21:53:05 +0200

    Utilities.js

--- Utilities.js
+++ Utilities.js
@@ -2,17 +2,17 @@
  * Created by adi on 1/20/2017.
  */
 var Utils = {
-    locToTile : function(j , i) {
-        var x = j* gameOptions.tileSize;
-        var y = i* gameOptions.tileSize;
+    locToTile : function(i , j) {
+        var x = i* gameOptions.tileSize;
+        var y = j* gameOptions.tileSize;
         if (x > gameOptions.gameWidth)
             x = gameOptions.gameWidth;
         if (y > gameOptions.gameHeight)
             y = gameOptions.gameHeight;
-        return [x, y ];
+        return {'x': x, 'y': y};
     },
 
     tileToLoc : function (x, y) {
-        return[Math.floor( x / gameOptions.tileSize) , Math.floor( y / gameOptions.tileSize)];
+        return {'i' : Math.floor( x / gameOptions.tileSize) , 'j': Math.floor( y / gameOptions.tileSize)};
     }
-}
+} 
