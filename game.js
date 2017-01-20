65c2b7b320b01b48b447374db2a2e4ebbebe1a4e
adipie <adipiek@gmail.com>
2017-01-20 20:23:20 +0200

    return locToTile

--- game.js
+++ game.js
@@ -99,4 +99,15 @@ window.onload = function () {
         }
     }
 
+
+    function locToTile(j , i) {
+        return [j* gameOptions.tileSize, i* gameOptions.tileSize ];
+    }
+
+    function tileToLoc(x, y) {
+        return[x / gameOptions.tileSize, y / gameOptions.tileSize];
+    }
+
+
+
 };
