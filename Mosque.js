c546310929ada254cdb5b06c2df4066c49ed02a3
Erez Bursztyn <erez@slatescience.com>
2017-01-20 18:46:20 +0200

    wave animation

--- Mosque.js
+++ Mosque.js
@@ -8,6 +8,14 @@ Mosque.prototype.constructor = Mosque;
 
 Mosque.prototype.sendWaves = function() {
 	console.log("MOSQUE SENDING WAVES");
+	var smallWave = this.game.make.sprite(this.width/2, 0, "mosque_wave_small");
+	var bigWave = this.game.make.sprite(this.width/2, 0, "mosque_wave_big");
+    smallWave.anchor.setTo(0.5, 0.5);
+    smallWave.scale.setTo(0.0, 0.0);
+    this.addChild(smallWave);
+    var tween = this.game.add.tween(smallWave.scale);
+    tween.to({ x: 1.0, y: 1.0 }, 100);
+    tween.start();
 };
 
 
